3c78c168ac4b92f1edb2db4ee641ca53f5684e10
Anelise Bercea <anelise.bercea@gmail.com>
2022-11-11 11:29:30 +0200

    laborator5

---
+++ Models/LibraryViewModels/PublishedBookData.cs
@@ -0,0 +1,10 @@
+﻿namespace Bercea_Anelise_Laborator2_MPA.Models.LibraryViewModels
+{
+    public class PublishedBookData
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public bool IsPublished { get; set; }
+
+    }
+}
