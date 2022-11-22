ab555ef4d37bac1295cd53d329eb256b8885c1c2
Anelise Bercea <anelise.bercea@gmail.com>
2022-11-22 19:43:18 +0200

    lab7

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,17 +16,52 @@ namespace Bercea_Anelise_Laborator2_MPA.Controllers
     public class CustomersController : Controller
     {
         private readonly LibraryContext _context;
-
+        private string _baseUrl = "http://localhost:5128/api/Customers";
         public CustomersController(LibraryContext context)
         {
             _context = context;
         }
 
+        public async Task<ActionResult> Index()
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync(_baseUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var customers = JsonConvert.DeserializeObject<List<Customer>>(await response.Content.ReadAsStringAsync());
+                return View(customers);
+            }
+            return NotFound();
+
+        }
+
+        // GET: Inventory/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new BadRequestResult();
+            }
+            var client = new HttpClient();
+            var response = await client.GetAsync($"{_baseUrl}/{id.Value}");
+            if (response.IsSuccessStatusCode)
+            {
+                var customer = JsonConvert.DeserializeObject<Customer>(
+                await response.Content.ReadAsStringAsync());
+                return View(customer);
+            }
+            return NotFound();
+        }
+
+
+        /*
         // GET: Customers
         public async Task<IActionResult> Index()
         {
               return View(await _context.Customers.ToListAsync());
         }
+        
 
         // GET: Customers/Details/5
         public async Task<IActionResult> Details(int? id)
@@ -42,7 +80,7 @@ namespace Bercea_Anelise_Laborator2_MPA.Controllers
 
             return View(customer);
         }
-
+*/
         // GET: Customers/Create
         public IActionResult Create()
         {
@@ -52,6 +90,7 @@ namespace Bercea_Anelise_Laborator2_MPA.Controllers
         // POST: Customers/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        /*
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("CustomerID,Name,Adress,BirthDate")] Customer customer)
@@ -64,7 +103,48 @@ namespace Bercea_Anelise_Laborator2_MPA.Controllers
             }
             return View(customer);
         }
+        */
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind("CustomerID,Name,Adress,BirthDate")]Customer customer)
+        {
+            if (!ModelState.IsValid) return View(customer);
+            try
+            {
+                var client = new HttpClient();
+                string json = JsonConvert.SerializeObject(customer);
+                var response = await client.PostAsync(_baseUrl,
+                new StringContent(json, Encoding.UTF8, "application/json"));
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, $"Unable to create record:{ ex.Message}");
+            }
+            return View(customer);
+        }
 
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new BadRequestResult();
+            }
+            var client = new HttpClient();
+            var response = await client.GetAsync($"{_baseUrl}/{id.Value}");
+            if (response.IsSuccessStatusCode)
+            {
+                var customer = JsonConvert.DeserializeObject<Customer>(
+                await response.Content.ReadAsStringAsync());
+                return View(customer);
+            }
+            return new NotFoundResult();
+        }
+
+        /*
         // GET: Customers/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
@@ -80,7 +160,7 @@ namespace Bercea_Anelise_Laborator2_MPA.Controllers
             }
             return View(customer);
         }
-
+       
         // POST: Customers/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -115,7 +195,25 @@ namespace Bercea_Anelise_Laborator2_MPA.Controllers
             }
             return View(customer);
         }
+        */
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind("CustomerID,Name,Adress,BirthDate")]
+Customer customer)
+        {
+            if (!ModelState.IsValid) return View(customer);
+            var client = new HttpClient();
+            string json = JsonConvert.SerializeObject(customer);
+            var response = await client.PutAsync($"{_baseUrl}/{customer.CustomerID}",
+            new StringContent(json, Encoding.UTF8, "application/json"));
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+        /*
         // GET: Customers/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
@@ -133,7 +231,25 @@ namespace Bercea_Anelise_Laborator2_MPA.Controllers
 
             return View(customer);
         }
+        */
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new BadRequestResult();
+            }
+            var client = new HttpClient();
+            var response = await client.GetAsync($"{_baseUrl}/{id.Value}");
+            if (response.IsSuccessStatusCode)
+            {
+                var customer = JsonConvert.DeserializeObject<Customer>(await
+               response.Content.ReadAsStringAsync());
+                return View(customer);
+            }
+            return new NotFoundResult();
+        }
 
+        /*
         // POST: Customers/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
@@ -152,10 +268,38 @@ namespace Bercea_Anelise_Laborator2_MPA.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+        */
+        // POST: Customers/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete([Bind("CustomerID")] Customer customer)
+        {
+            try
+            {
+                var client = new HttpClient();
+                HttpRequestMessage request =
+                new HttpRequestMessage(HttpMethod.Delete,
+               $"{_baseUrl}/{customer.CustomerID}")
+                {
+                    Content = new StringContent(JsonConvert.SerializeObject(customer),
+               Encoding.UTF8, "application/json")
+                };
+                var response = await client.SendAsync(request);
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, $"Unable to delete record: { ex.Message} ");
+            }
+            return View(customer);
+        }
+    }
 
-        private bool CustomerExists(int id)
+    /*
+    private bool CustomerExists(int id)
         {
           return _context.Customers.Any(e => e.CustomerID == id);
         }
     }
+*/
 }
